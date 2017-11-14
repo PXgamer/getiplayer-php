a17199f55f801821574af73f2d9eae557fe7fbe6
pxgamer <owzie123@gmail.com>
2017-11-14 23:02:34 +0000

    Added namespace for tests

--- tests/MainTest.php
+++ tests/MainTest.php
@@ -1,19 +1,20 @@
 <?php
 
+namespace pxgamer\GetiPlayer;
+
 use PHPUnit\Framework\TestCase;
-use pxgamer\GetiPlayer;
 
 class MainTest extends TestCase
 {
     public function testCanBeInitialised()
     {
-        $client = new GetiPlayer\Client();
-        $this->assertInstanceOf(GetiPlayer\Client::class, $client);
+        $client = new Client();
+        $this->assertInstanceOf(Client::class, $client);
     }
 
     public function testCanGetVideoId()
     {
-        $client = new GetiPlayer\Client();
+        $client = new Client();
         $client->setUrl('http://www.bbc.co.uk/iplayer/episode/b088ppll/sherlock-series-4-2-the-lying-detective');
         $this->assertSame('b088pnjk', $client->getVideoId());
     }
